4c56394b066b2bc48bf3b6aa336dbaad578b6265
daniyal ali <daniyalali12568@gmail.com>
2024-06-20 19:08:43 +0500

    simple/nested/group/dynamic/Routes

---
+++ src/app/Course/Java/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const java  = () => {
+  return (
+    <div> 
+      java 
+    </div>
+  )
+}
+
+export default java 
---
+++ src/app/Route/NestRouteFolder/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Nest Route Folder Page</div>
+  )
+}
+
+export default page
